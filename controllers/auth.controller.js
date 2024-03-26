ca308e2a037a47374152cc8fdfb6ef5b547c3f09
karanch577 <karandemo577@gmail.com>
2024-03-26 12:56:40 +0530

    cookie option updated

--- controllers/auth.controller.js
+++ controllers/auth.controller.js
@@ -9,9 +9,9 @@ import crypto from "crypto";
 const cookieOptions = {
     expires: new Date(Date.now() + 3 * 24 * 60 * 60 * 1000),
     httpOnly: true,
-    sameSite: 'none',
+    sameSite: 'lax',
     secure: true,
-    domain: '.vercel.app'
+    // domain: '.vercel.app'
 }
 
 /***************************************************************
