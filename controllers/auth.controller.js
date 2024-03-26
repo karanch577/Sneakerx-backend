b30a6e5cab175e84a5b81697e1452a7f22cf25c8
karanch577 <karandemo577@gmail.com>
2024-03-26 12:33:23 +0530

    subdomain config added in cookie options

--- controllers/auth.controller.js
+++ controllers/auth.controller.js
@@ -10,7 +10,8 @@ const cookieOptions = {
     expires: new Date(Date.now() + 3 * 24 * 60 * 60 * 1000),
     httpOnly: true,
     sameSite: 'none',
-    secure: true
+    secure: true,
+    domain: '.vercel.app'
 }
 
 /***************************************************************
