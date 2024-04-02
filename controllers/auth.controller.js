273dc1358693aca3ff5e05da09850f112a3adfdd
karanch577 <karandemo577@gmail.com>
2024-04-02 11:56:34 +0530

    updated site config in cookie options

--- controllers/auth.controller.js
+++ controllers/auth.controller.js
@@ -10,6 +10,8 @@ const cookieOptions = {
     expires: new Date(Date.now() + 3 * 24 * 60 * 60 * 1000),
     httpOnly: true,
     secure: true,
+    domain: 'sneakerx.xyz',
+    sameSite: 'None',
 }
 
 /***************************************************************
