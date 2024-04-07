b7c9726c5a31fb132195b7eb8a6b254040cfc09e
karanch577 <karandemo577@gmail.com>
2024-04-07 13:19:54 +0530

    cookie option for subdomain added

--- controllers/auth.controller.js
+++ controllers/auth.controller.js
@@ -10,7 +10,7 @@ const cookieOptions = {
     expires: new Date(Date.now() + 3 * 24 * 60 * 60 * 1000),
     httpOnly: true,
     secure: true,
-    domain: 'sneakerx.xyz',
+    domain: '.sneakerx.xyz',
     sameSite: 'None',
 }
 
